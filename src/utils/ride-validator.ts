1bc579017532e87e744a877586532c2370963aa3
ahd3r <ander11110@gmail.com>
2022-12-29 12:22:08 +0100

    fix

--- src/utils/ride-validator.ts
+++ src/utils/ride-validator.ts
@@ -15,13 +15,15 @@ export const addPassenger = z.object({
   userId: z.string()
 });
 
-export const updateRideData = z.object({
-  driverId: z.string().uuid().optional(),
-  availablePlaces: z.number().min(1).optional(),
-  carId: z.string().uuid().optional(),
-  from: z.string().optional(),
-  to: z.string().optional(),
-  distanceMeters: z.number().min(1).optional(),
-  timeMinutes: z.number().min(1).optional(),
-  description: z.string().optional()
-});
+export const updateRideData = z
+  .object({
+    driverId: z.string().uuid().optional(),
+    availablePlaces: z.number().min(1).optional(),
+    carId: z.string().uuid().optional(),
+    from: z.string().optional(),
+    to: z.string().optional(),
+    distanceMeters: z.number().min(1).optional(),
+    timeMinutes: z.number().min(1).optional(),
+    description: z.string().optional()
+  })
+  .optional();
