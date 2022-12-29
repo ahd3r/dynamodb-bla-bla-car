aeb8514e95aed3b14f6f301f864c5aaf882229b5
ahd3r <ander11110@gmail.com>
2022-12-29 12:09:44 +0100

    fix

--- src/utils/ride-validator.ts
+++ src/utils/ride-validator.ts
@@ -1,33 +1,27 @@
 import { z } from 'zod';
 
-export const createRideData = z
-  .object({
-    driverId: z.string().uuid(),
-    availablePlaces: z.number().min(1),
-    carId: z.string().uuid(),
-    from: z.string(),
-    to: z.string(),
-    distanceMeters: z.number().min(1),
-    timeMinutes: z.number().min(1),
-    description: z.string().optional()
-  })
-  .required();
+export const createRideData = z.object({
+  driverId: z.string().uuid(),
+  availablePlaces: z.number().min(1),
+  carId: z.string().uuid(),
+  from: z.string(),
+  to: z.string(),
+  distanceMeters: z.number().min(1),
+  timeMinutes: z.number().min(1),
+  description: z.string().optional()
+});
 
-export const addPassenger = z
-  .object({
-    userId: z.string()
-  })
-  .required();
+export const addPassenger = z.object({
+  userId: z.string()
+});
 
-export const updateRideData = z
-  .object({
-    driverId: z.string().uuid().optional(),
-    availablePlaces: z.number().min(1).optional(),
-    carId: z.string().uuid().optional(),
-    from: z.string().optional(),
-    to: z.string().optional(),
-    distanceMeters: z.number().min(1).optional(),
-    timeMinutes: z.number().min(1).optional(),
-    description: z.string().optional()
-  })
-  .required();
+export const updateRideData = z.object({
+  driverId: z.string().uuid().optional(),
+  availablePlaces: z.number().min(1).optional(),
+  carId: z.string().uuid().optional(),
+  from: z.string().optional(),
+  to: z.string().optional(),
+  distanceMeters: z.number().min(1).optional(),
+  timeMinutes: z.number().min(1).optional(),
+  description: z.string().optional()
+});
